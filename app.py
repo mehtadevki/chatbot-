705fa80bc601f1fd33ab61eff57a6c1d5a745f17
mehtadevki <devkimehta21nov729@gmail.com>
2025-10-07 18:21:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template,request, jsonify
 
 app = Flask(__name__)
 
@@ -48,3 +48,4 @@ def chat():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
